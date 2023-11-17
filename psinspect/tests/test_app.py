bb235c763b59153dc7e5ac5d96646251c46ade46
Xavier Garrido <xavier.garrido@gmail.com>
2023-11-17 09:35:35 +0100

    add test for bunch class

--- psinspect/tests/test_app.py
+++ psinspect/tests/test_app.py
@@ -4,6 +4,16 @@ from psinspect import App
 
 
 class SOPsinspectTests(unittest.TestCase):
+    def test_bunch(self):
+        from psinspect.app import Bunch
+
+        b = Bunch(foo="bar")
+        self.assertEqual(b.foo, "bar")
+        with self.assertRaises(AttributeError):
+            b.bar
+        b.update(name="John Doe")
+        self.assertEqual(b.name, "John Doe")
+
     def test_app(self):
         my_app = App()
         my_app.initialize()
